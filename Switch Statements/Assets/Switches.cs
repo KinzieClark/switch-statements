fb37dd9d2cccdc2811ece95774a9507a89f5f58d
Unknown <kinzie.colleen@gmail.com>
2017-02-10 10:53:51 -0700

    Added Switch Statement

---
+++ Switch Statements/Assets/Switches.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Switches : MonoBehaviour {
+
+    public enum ColorsChoices {RED, GREEN, BLUE};
+    public ColorsChoices playerColor;
+
+    public enum GameStates
+    {
+        LOADING,
+        STARTING,
+        PLAYING,
+        ENDING
+    }
+
+    public GameStates currentGameState = GameStates.LOADING;
+
+	// Use this for initialization
+	void Start () {
+	
+        switch (playerColor)
+        {
+            case ColorsChoices.GREEN:
+                print("This gives access to all weapons");
+                break;
+
+            case ColorsChoices.RED:
+                print("This gives access to all power-ups");
+                break;
+
+            case ColorsChoices.BLUE:
+                print("Just Dies");
+                break;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
