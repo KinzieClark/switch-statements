81327b95bb68c76268d44153f4a7d84975f76ec9
Unknown <kinzie.colleen@gmail.com>
2017-02-13 10:49:15 -0700

    Added ifToSwitch and Loops

---
+++ Switch Statements/Assets/Loops.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Loops : MonoBehaviour {
+
+    public string[] animals;
+
+    public string animal = "Wolf";
+
+	// Use this for initialization
+	void Start () {
+        foreach (string animalInAnimals in animals)
+        {
+            if(animal == animalInAnimals)
+            {
+                print("I have a match, it is " + animalInAnimals);
+            }
+            else
+            {
+                print(animalInAnimals + " is not a match");
+            }
+        }
+	}
+	
+}
---
+++ Switch Statements/Assets/ifToSwitch.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ifToSwitch : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        //Check for truth with one object
+        if(2+2 == 4)
+        {
+            print(true);
+        }
+        if(2+2+2 != 5)//!= is checking if something is false
+        {
+            print(true);
+        }
+
+        int nums = 2 + 2;
+	    //check for true and false in a single statement
+        if(nums == 5)
+        {
+            print(true);
+        }
+
+        else
+        {
+            print(false);
+        }
+        //Check for three of more values;
+        switch (nums)
+        {
+            case 6:
+                print("Well done, you have " + nums);
+                break;
+            case 7:
+                print("Too bad, you have " + nums);
+                break;
+            case 8:
+                print("Stop drilling, you have " + nums);
+                break;
+            default:
+                print(nums);
+                break;
+        }
+	}
+	
+}
